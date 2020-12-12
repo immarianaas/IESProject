64a98545fd37ab7a07d98e25a0d68734fb9b8d93
ritamf <47431749+ritamf@users.noreply.github.com>
2020-12-12 23:04:46 +0000

    Update sender.py
    - List `queues` no longer exists
    - Convert dictionary to json string, in body argument, in RabbitMQ publish

--- sender.py
+++ sender.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import pika
-from random import random, randint
+from random import random, randint, choice
 from time import sleep
+import json
 
 import fake_data_generators_nd as gen
 
@@ -10,32 +11,30 @@ def main():
 	connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 	channel = connection.channel()
 
-	# in case there's only one queue, comment lines 18-24 and uncomment this:
+	# if there's only one queue, comment the queue2gen and for loop and uncomment this:
+	# generators = [getattr(gen, f) for f in dir(gen) if f.startswith("fake_")]
 	# channel.queue_declare(queue='queue')
-	# generators = [gen.fake_co2, gen.fake_body_temp, gen.fake_people_counter]
+
+	queue2gen = {
+		f.replace("fake","queue") : getattr(gen, f) 
+			for f in dir(gen)
+				if f.startswith("fake_")
+	}
+
+	for q in queue2gen:
+		channel.queue_declare(queue=q)
 	
-	# every queue name must start with queue!
-	queues=['queue_co2', 'queue_body_temp', 'queue_people_counter']
-	for i in range(len(queues)):
-		channel.queue_declare(queue=queues[i])
-
-	queue2gen = dict()
-	for q in queues:
-		queue2gen[q]=getattr(gen, "fake"+q[5:])
-
 	while True:
-
 		if randint(0,1):
-			# comment these 2 lines if only one queue is used:
-			random_queue = queues[randint(0,len(queues)-1)]
+			random_queue = choice(list(queue2gen.keys()))
 			random_generator = queue2gen[random_queue]
-			
+
 			channel.basic_publish(
 				exchange='', 
 				routing_key=random_queue, # or 'queue', if there's only one queue
-				body=random_generator() # or generators[randint(0, len(generators))], if there's only one queue
+				body=json.dumps(random_generator()) # or generators[randint(0, len(generators))], if there's only one queue
 			)
-			
+
 		sleep(random()*5)
 
 	connection.close()
